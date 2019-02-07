bbd4526244813ac472cda7239bc5315c623309ec
Vlad.Filinsky <vfilinsky.sells@gmail.com>
2019-02-07 17:12:53 +0500

    slider

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 $(function() {
-
-	// Custom JS
-
+	     $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:false,
+        center:true,
+        dots:false,
+      	animateOut: 'fadeOut',
+    		animateIn: 'fadeIn',
+    		smartSpeed: 450,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: '0'
+    });
+/* HOME SELECT "APARTMENT PAGE" */
+$('.preview-link:first').addClass('preview-link_active');
+$('.preview-link').on('click', function() {
+  $('.preview-link').removeClass('preview-link_active');
+  $(this).addClass('preview-link_active');
+});
 });
