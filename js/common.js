0d37801c184c696b7a3655f09616195d58e96932
Vlad.Filinsky <vfilinsky.sells@gmail.com>
2019-02-14 19:44:34 +0500

    comment

--- js/common.js
+++ js/common.js
@@ -14,7 +14,7 @@ $(function() {
         autoplayHoverPause:true,
         startPosition: '0'
     });
-/* HOME SELECT "APARTMENT PAGE" */
+/* ховер на активном слайде */
 $('.preview-link:first').addClass('preview-link_active');
 $('.preview-link').on('click', function() {
   $('.preview-link').removeClass('preview-link_active');
@@ -32,10 +32,21 @@ $('.callback-close-modal').click(function(){
 /* Модальное окно "Абсолютная рассрочка" */
 var elements_buy = $('.buy-overlay, .buy-modal');
 $('.buy-box_btn').click(function(){
-    elements_buy.addClass('active');
+ var title = $(this).attr('data-title');
+ var text = $(this).parent().find('.buy-box_text').text();
+ $('div.buy-modal-content').find('h4.js-popup-title').text(title);
+ $('div.buy-modal-content').find('p.js-popup-text').text(text);
+  elements_buy.addClass('active');
 });
 $('.buy-close-modal').click(function(){
     elements_buy.removeClass('active');
 });
-
+/* Закрываем попап по клику вне окна */
+  $(document).mouseup(function (e) {
+    var popup = $('.modal-content');
+    var element = $('.modal-overlay, .modal');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    element.removeClass('active');
+    }
+  });
 });
